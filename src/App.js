a700fc282dc6e10563e82486fc1f092e473e12db
J3eff <jeffersonvideo125@gmail.com>
2021-12-27 10:51:42 -0300

    Layout e containers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Component } from 'react';
-import { Fragment } from 'react/cjs/react.production.min';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container } from '@material-ui/core'
 
 export default class App extends Component {
  
  render() {
     return (
-      <Fragment>
+      <Container component="article" maxWidth="sm"> 
         <h1>Formulário de cadastro</h1>
         <FormularioCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
