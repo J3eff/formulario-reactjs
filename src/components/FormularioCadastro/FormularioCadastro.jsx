895e48da47a8a10e012283d4f376910b597b52cf
J3eff <jeffersonvideo125@gmail.com>
2022-01-11 23:53:13 -0300

    Renderização condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,17 +2,33 @@ import React, { useState } from 'react';
 import DadosUsuario from './DadosUsuario';
 import DadosPessoais from './DadosPessoais';
 import DadosEntrega from './DadosEntrega';
-
-
+import { Typography } from '@material-ui/core';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [etapaAtual, setEtapaAtual] = useState(0);
+
+    function formularioAtual(etapa) {
+        switch (etapa) {
+            case 0:
+                return <DadosUsuario />
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+            case 2:
+                return <DadosEntrega />
+            default:
+                return <Typography>Erro ao selecionar formulario</Typography>
+        }
+    }
+
     return (
         <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-            <DadosUsuario />
-            <DadosEntrega />
+            { formularioAtual(etapaAtual) }
+            {/* <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} /> */}
+            {/* <DadosUsuario /> */}
+            {/* <DadosEntrega /> */}
         </>
     );
 }
+
 
 export default FormularioCadastro;
